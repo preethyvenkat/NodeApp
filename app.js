0721641e430c9b829da8683ae1766d7e1b9752d9
Raja Sundaram <sundarsy@gmail.com>
2019-10-17 17:16:32 +0800

    adding modificatins

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<title>My Example</title>
-
-
-<p id="msg"></p>
-
-<script>
-  var citiesAU =  ["Sydney", "Melbourne", "Brisbane", "Perth"],
-      citiesUS =  ["New York", "Miami", "San Fransisco", "Los Angeles"],
-      cities = citiesAU.concat(citiesUS);
-  document.getElementById("msg").innerHTML = cities;
-</script>
+//importing node framework
+var express = require(‘express’);
+ 
+var app = express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get(‘/’, function (req, res) {
+ res.send(‘hello world’);
+});
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
